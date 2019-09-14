3c11a04bcabd867ae61891eef0f8a3f2d753473f
SRTigers98 <ben-eder@freenet.de>
2019-09-14 10:30:18 +0200

    Removed english locale (YAGNI)

--- src/i18n.js
+++ src/i18n.js
@@ -17,7 +17,7 @@ function loadLocaleMessages() {
 }
 
 export default new VueI18n({
-    locale: navigator.language.substr(0, 2) || 'en',
-    fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+    locale: navigator.language.substr(0, 2) || 'de',
+    fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'de',
     messages: loadLocaleMessages()
 });
