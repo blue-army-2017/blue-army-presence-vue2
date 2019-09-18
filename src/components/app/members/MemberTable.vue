e29b184965ac1e579dff55d582c4a11e5ef5349a
SRTigers98 <ben-eder@freenet.de>
2019-09-18 17:56:43 +0200

    Member table as component

---
+++ src/components/app/members/MemberTable.vue
@@ -0,0 +1,53 @@
+<template>
+    <md-table md-card class="member-table">
+        <md-table-toolbar>
+            <h1>{{ title }}</h1>
+        </md-table-toolbar>
+
+        <md-table-row>
+            <md-table-head>{{ $t('app.members.header-last-name') }}</md-table-head>
+            <md-table-head>{{ $t('app.members.header-first-name') }}</md-table-head>
+            <md-table-head>{{ $t('app.members.header-edit') }}</md-table-head>
+            <md-table-head>{{ $t('app.members.header-delete') }}</md-table-head>
+        </md-table-row>
+
+        <md-table-row :key="member.id" v-for="member in members">
+            <md-table-cell>{{ member.lastName }}</md-table-cell>
+            <md-table-cell>{{ member.firstName }}</md-table-cell>
+            <md-table-cell>
+                <md-button class="md-icon-button">
+                    <md-icon>edit</md-icon>
+                </md-button>
+            </md-table-cell>
+            <md-table-cell>
+                <md-button class="md-icon-button">
+                    <md-icon>delete</md-icon>
+                </md-button>
+            </md-table-cell>
+        </md-table-row>
+    </md-table>
+</template>
+
+<script>
+    export default {
+        data: () => ({}),
+        props: {
+            title: {
+                type: String,
+                required: true
+            },
+            members: {
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .member-table {
+        max-width: 95%;
+        flex-wrap: nowrap;
+        overflow: scroll;
+    }
+</style>
--- src/views/app/menu/Members.vue
+++ src/views/app/menu/Members.vue
@@ -1,36 +1,12 @@
 <template>
     <md-content class="member-content">
-        <md-table md-card class="member-table">
-            <md-table-toolbar>
-                <h1>{{ $t('app.menu.members.title-active-members') }}</h1>
-            </md-table-toolbar>
-
-            <md-table-row>
-                <md-table-head>Name</md-table-head>
-                <md-table-head>Vorname</md-table-head>
-                <md-table-head>Bearbeiten</md-table-head>
-                <md-table-head>Löschen</md-table-head>
-            </md-table-row>
-
-            <md-table-row :key="member.id" v-for="member in members">
-                <md-table-cell>{{ member.lastName }}</md-table-cell>
-                <md-table-cell>{{ member.firstName }}</md-table-cell>
-                <md-table-cell>
-                    <md-button class="md-icon-button">
-                        <md-icon>edit</md-icon>
-                    </md-button>
-                </md-table-cell>
-                <md-table-cell>
-                    <md-button class="md-icon-button">
-                        <md-icon>delete</md-icon>
-                    </md-button>
-                </md-table-cell>
-            </md-table-row>
-        </md-table>
+        <member-table :title="$t('app.menu.members.title-active-members')" :members="members" />
     </md-content>
 </template>
 
 <script>
+    import { MemberTable } from '../../../components';
+
     export default {
         data: () => ({
             members: [
@@ -38,7 +14,10 @@
                 {id: '2', firstName: 'Maria', lastName: 'Musterfrau'},
                 {id: '3', firstName: 'Tom', lastName: 'Meier'}
             ]
-        })
+        }),
+        components: {
+            MemberTable
+        }
     }
 </script>
 
@@ -48,11 +27,5 @@
         flex-direction: row;
         flex-wrap: wrap;
         justify-content: space-evenly;
-    }
-
-    .member-table {
-        max-width: 95%;
-        flex-wrap: nowrap;
-        overflow: scroll;
     }
 </style>
