a5746f5edd74f6a1109bfe17f3d9b63b7a3f3e03
SRTigers98 <ben-eder@freenet.de>
2019-09-18 17:38:56 +0200

    Member overview with table

--- src/views/app/menu/Members.vue
+++ src/views/app/menu/Members.vue
@@ -1,37 +1,58 @@
 <template>
     <md-content class="member-content">
-        <md-list>
-            <md-subheader>Mitglieder</md-subheader>
-            <md-list-item :key="member.id" v-for="member in members" @click="member.menuOpen = !member.menuOpen">
-                <md-menu md-direction="bottom-start" :md-active.sync="member.menuOpen">
-                    <span>{{ member.lastName + ', ' + member.firstName }}</span>
-                    <md-menu-content>
-                        <md-menu-item>
-                            <md-icon>edit</md-icon>
-                            <span>Bearbeiten</span>
-                        </md-menu-item>
-                        <md-menu-item>
-                            <md-icon>delete</md-icon>
-                            <span>Löschen</span>
-                        </md-menu-item>
-                    </md-menu-content>
-                </md-menu>
-            </md-list-item>
-        </md-list>
+        <md-table md-card class="member-table">
+            <md-table-toolbar>
+                <h1>{{ $t('app.menu.members.title-active-members') }}</h1>
+            </md-table-toolbar>
+
+            <md-table-row>
+                <md-table-head>Name</md-table-head>
+                <md-table-head>Vorname</md-table-head>
+                <md-table-head>Bearbeiten</md-table-head>
+                <md-table-head>Löschen</md-table-head>
+            </md-table-row>
+
+            <md-table-row :key="member.id" v-for="member in members">
+                <md-table-cell>{{ member.lastName }}</md-table-cell>
+                <md-table-cell>{{ member.firstName }}</md-table-cell>
+                <md-table-cell>
+                    <md-button class="md-icon-button">
+                        <md-icon>edit</md-icon>
+                    </md-button>
+                </md-table-cell>
+                <md-table-cell>
+                    <md-button class="md-icon-button">
+                        <md-icon>delete</md-icon>
+                    </md-button>
+                </md-table-cell>
+            </md-table-row>
+        </md-table>
     </md-content>
 </template>
 
 <script>
     export default {
         data: () => ({
-            members: [{id: '1', firstName: 'Max', lastName: 'Mustermann', menuOpen: false}],
+            members: [
+                {id: '1', firstName: 'Max', lastName: 'Mustermann'},
+                {id: '2', firstName: 'Maria', lastName: 'Musterfrau'},
+                {id: '3', firstName: 'Tom', lastName: 'Meier'}
+            ]
         })
     }
 </script>
 
 <style scoped>
     .member-content {
-        margin-left: auto;
-        margin-right: auto;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+    }
+
+    .member-table {
+        max-width: 95%;
+        flex-wrap: nowrap;
+        overflow: scroll;
     }
 </style>
