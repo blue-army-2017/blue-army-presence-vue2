3b00884b3772b2211e696bbdea27da744beb75b3
SRTigers98 <ben-eder@freenet.de>
2019-09-18 16:51:26 +0200

    Login view with flexbox

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,22 +1,24 @@
 <template>
-    <md-card class="login-card">
-        <md-card-header>{{ $t('auth.login.header') }}</md-card-header>
-        <md-card-content>
-            <md-field class="login-field">
-                <label>{{ $t('auth.login.email') }}</label>
-                <md-input class="login-input" v-model="email" @keypress.enter="login"/>
-            </md-field>
-            <md-field class="login-field">
-                <label>{{ $t('auth.login.password') }}</label>
-                <md-input class="login-input" type="password" v-model="password" @keypress.enter="login"/>
-            </md-field>
-            <md-button class="login-button md-raised" @click="login" :disabled="email === '' || password === ''">
-                {{ $t('auth.login.loginButton') }}
-            </md-button>
-        </md-card-content>
-        <md-dialog-alert :md-active.sync="loginError" :md-content="$t('auth.login.errorDialog.content')"
-                         :md-confirm-text="$t('auth.login.errorDialog.confirm')"/>
-    </md-card>
+    <md-content class="login-content">
+        <md-card class="login-card">
+            <md-card-header>{{ $t('auth.login.header') }}</md-card-header>
+            <md-card-content class="login-card-content">
+                <md-field class="login-field">
+                    <label>{{ $t('auth.login.email') }}</label>
+                    <md-input class="login-input" v-model="email" @keypress.enter="login"/>
+                </md-field>
+                <md-field class="login-field">
+                    <label>{{ $t('auth.login.password') }}</label>
+                    <md-input class="login-input" type="password" v-model="password" @keypress.enter="login"/>
+                </md-field>
+                <md-button class="login-button md-raised" @click="login" :disabled="email === '' || password === ''">
+                    {{ $t('auth.login.loginButton') }}
+                </md-button>
+            </md-card-content>
+            <md-dialog-alert :md-active.sync="loginError" :md-content="$t('auth.login.errorDialog.content')"
+                             :md-confirm-text="$t('auth.login.errorDialog.confirm')"/>
+        </md-card>
+    </md-content>
 </template>
 
 <script>
@@ -40,29 +42,32 @@
 </script>
 
 <style scoped>
+    .login-content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
     .login-card {
-        top: calc((100% - 266px/* height of card */) / 2);
-        margin-left: auto;
-        margin-right: auto;
         width: 80%;
         max-width: 400px;
         text-align: center;
     }
 
+    .login-card-content {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
     .login-field {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
         width: 80%;
     }
 
     .login-input {
         width: 100%;
-    }
-
-    .login-button {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
     }
 </style>
