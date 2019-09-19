3bc110c11e56f1e1bb6c94cbd866e55df014c323
SRTigers98 <ben-eder@freenet.de>
2019-09-19 19:17:19 +0200

    Components for EditDialog

--- src/components/app/members/EditMemberDialog.vue
+++ src/components/app/members/EditMemberDialog.vue
@@ -1,7 +1,22 @@
 <template>
-    <md-dialog :md-active.sync="active">
+    <md-dialog :md-active.sync="active" class="edit-member-dialog">
         <md-dialog-title v-if="create">{{ $t('app.members.dialog.edit.title-add') }}</md-dialog-title>
         <md-dialog-title v-else>{{ $t('app.members.dialog.edit.title-edit') }}</md-dialog-title>
+
+        <md-field>
+            <label>{{ $t('app.members.table.header-first-name') }}</label>
+            <md-input v-model="firstName" maxlength="30" />
+        </md-field>
+        <md-field>
+            <label>{{ $t('app.members.table.header-last-name') }}</label>
+            <md-input v-model="lastName" maxlength="30" />
+        </md-field>
+
+        <md-dialog-actions>
+            <md-button @click="active = false">{{ $t('app.members.dialog.edit.button-cancel') }}</md-button>
+            <md-button v-if="create">{{ $t('app.members.dialog.edit.button-confirm-add') }}</md-button>
+            <md-button v-else>{{ $t('app.members.dialog.edit.button-confirm-edit') }}</md-button>
+        </md-dialog-actions>
     </md-dialog>
 </template>
 
@@ -10,7 +25,9 @@
 
     export default {
         data: () => ({
-            active: false
+            active: false,
+            firstName: '',
+            lastName: ''
         }),
         props: {
             eventName: {
@@ -31,4 +48,7 @@
 </script>
 
 <style scoped>
+    .edit-member-dialog {
+        padding: 0 10px;
+    }
 </style>
