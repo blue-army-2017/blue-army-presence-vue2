207b03ad87b1a5656b6294f72db48d2fd9df6ed7
SRTigers98 <ben-eder@freenet.de>
2019-10-04 17:38:36 +0200

    Get season entry by the season id and display the title

--- src/views/app/game/SeasonGames.vue
+++ src/views/app/game/SeasonGames.vue
@@ -1,16 +1,29 @@
 <template>
     <md-content>
-        <h1>{{ seasonId }}</h1>
+        <h1>{{ seasonName }}</h1>
     </md-content>
 </template>
 
 <script>
+    import { getSeasonRef } from '../../../api';
+
     export default {
-        data: () => ({}),
+        data: () => ({
+            seasonName: '',
+            games: []
+        }),
         computed: {
             seasonId() {
                 return this.$route.params.seasonId;
             }
+        },
+        created() {
+            getSeasonRef().child(this.seasonId).on('value', snapshot => {
+                if (this.seasonName === '') {
+                    this.seasonName = this.$t('app.seasonGames.title', {name: snapshot.val().name});
+                }
+                this.games = snapshot.val().games;
+            });
         }
     }
 </script>
