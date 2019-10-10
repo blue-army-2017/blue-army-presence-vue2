37482ca1141993a8520931771c59a819ee4d857f
SRTigers98 <ben-eder@freenet.de>
2019-10-10 22:23:08 +0200

    First steps to add members to a game

--- src/components/app/game/PresentMembers.vue
+++ src/components/app/game/PresentMembers.vue
@@ -1,18 +1,68 @@
 <template>
-    <md-content>
-        <!-- todo implementation -->
+    <md-content v-if="activeMembersLoading" class="present-members-container">
+        <md-progress-spinner md-mode="indeterminate" />
+    </md-content>
+    <md-content v-else class="present-members-container">
+        <h4>{{ $t('app.game.presentMembers.addPresentMember') }}</h4>
+
+        <md-field>
+            <label>{{ $t('app.game.presentMembers.selectLabel') }}</label>
+            <md-select v-model="selectedMember">
+                <md-option v-for="member in activeMembers" :key="member.id" :value="member.id">
+                    {{ getMemberName(member) }}
+                </md-option>
+            </md-select>
+        </md-field>
+
+        <md-button class="md-raised" :disabled="selectedMember === ''">
+            {{ $t('app.game.presentMembers.addButton') }}
+        </md-button>
     </md-content>
 </template>
 
 <script>
+    import { getMemberRef } from '../../../api';
+
     export default {
         data: () => ({
-            activeMembers: [],
-            selectedMember: null,
-            presentMembers: []
-        })
+            selectedMember: '',
+            presentMembers: [],
+            activeMembersLoading: true,
+            membersSnapshot: null
+        }),
+        computed: {
+            activeMembers() {
+                let activeMembers = [];
+                this.membersSnapshot.forEach(member => {
+                    if (member.val().active) {
+                        activeMembers.push({
+                            id: member.key,
+                            lastName: member.val().lastName,
+                            firstName: member.val().firstName
+                        });
+                    }
+                });
+                return activeMembers;
+            }
+        },
+        methods: {
+            getMemberName(member) {
+                return `${member.lastName}, ${member.firstName}`;
+            }
+        },
+        created() {
+            getMemberRef().orderByChild('lastName').on('value', snapshot => {
+                this.membersSnapshot = snapshot;
+                this.activeMembersLoading = false;
+            });
+        }
     }
 </script>
 
 <style scoped>
+    .present-members-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 </style>
