8c761802d1b8ededba133a37ae339f32495c9174
SRTigers98 <ben-eder@freenet.de>
2019-10-19 09:01:34 +0200

    Replaced statistics placeholder view

---
+++ src/views/app/menu/Statistics.vue
@@ -0,0 +1,15 @@
+<template>
+    <md-content>
+        <h1>Statistiken</h1>
+    </md-content>
+</template>
+
+<script>
+    export default {
+        data: () => ({})
+    }
+</script>
+
+<style scoped>
+
+</style>
